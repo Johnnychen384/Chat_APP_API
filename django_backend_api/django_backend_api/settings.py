7f86913ed3c855cc5a71b098a97ad763dd0853e4
Johnny Chen <Johnnychen384@gmail.com>
2023-01-05 15:07:22 -0500

    sdad

--- django_backend_api/django_backend_api/settings.py
+++ django_backend_api/django_backend_api/settings.py
@@ -34,9 +34,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://dry-retreat-68955.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'dry-retreat-68955.herokuapp.com/']
 
 
 
